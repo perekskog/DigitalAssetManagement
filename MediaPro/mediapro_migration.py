80e682f90947985ad92ce01239e24b173573b18b
Per Ekskog <per.ekskog@gmail.com>
2019-10-30 22:37:54 +0100

    mediapro_migration Added missing error detection. Added more detailed command line arguments.

--- MediaPro/mediapro_migration.py
+++ MediaPro/mediapro_migration.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 import argparse
 import json
@@ -20,38 +20,71 @@ ns = {'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
 
 def get_props(sender_map, xmlfile):
     tree = ET.parse(xmlfile)
-    
+
     root = tree.getroot()
 
+    # Headline
     nheadline = len(root.findall('./rdf:RDF/rdf:Description/photoshop:Headline',ns))
     headline = ''
     if(nheadline==0):
         sys.stderr.write('{}: ERROR: Missing headline, replaced with {}\n'.format(xmlfile, default_headline))
+        if(error_is_fatal):
+            sys.exit(-1)
+        headline = default_headline
+    elif(nheadline>1):
+        sys.stderr.write('{}: ERROR: More than 1 headline, replaced with {}\n'.format(xmlfile, default_headline))
+        if(error_is_fatal):
+            sys.exit(-1)
         headline = default_headline
     else:
         headline = root.find('./rdf:RDF/rdf:Description/photoshop:Headline',ns).text
 
+    # Date created
     ndatecreated = len(root.findall('./rdf:RDF/rdf:Description/photoshop:DateCreated',ns))
     datecreated = ''
     if(ndatecreated==0):
         sys.stderr.write('{}: ERROR: Missing datecreated, replaced with {}\n'.format(xmlfile, default_datecreated))
+        if(error_is_fatal):
+            sys.exit(-1)
+        datecreated = default_datecreated
+    elif(ndatecreated>1):
+        sys.stderr.write('{}: ERROR: More than 1 datecreated, replaced with {}\n'.format(xmlfile, default_datecreated))
+        if(error_is_fatal):
+            sys.exit(-1)
         datecreated = default_datecreated
     else:
         datecreated = root.find('./rdf:RDF/rdf:Description/photoshop:DateCreated',ns).text
 
+    # Sender
+    # Require either 1 (sender) or two, where first is top level sender (to disregard).
+    # No support for several senders.
+    sender_text = []
+    sender_text_short = []
+
     senders = root.findall('./rdf:RDF/rdf:Description/dc:subject/rdf:Bag/rdf:li',ns)
-    #print(senders)
-    senders_text = []
-    senders_text_short = []
-    for sender in senders:
-        if(sender.text != 'Peer'):
-            if(sender.text in sender_map):
-                senders_text.append(sender.text)
-                senders_text_short.append(sender_map[sender.text])
-            else:
-                sys.stderr.write('{}: ERROR: Unmapped sender: {}, replaced with {}'.format(xmlfile, sender.text, default_sender))
-                senders_text.append(sender.text)
-                senders_text_short.append(default_sender)
+    nsenders = len(senders)
+    if(nsenders==0):
+        sys.stderr.write('{}: ERROR: Missing sender, replaced with {}\n'.format(xmlfile, default_sender))
+        if(error_is_fatal):
+            sys.exit(-1)
+    if(nsenders==1):
+        sender_text = senders[0].text
+    if(nsenders==2):
+        # Prefix (Peer, ...) is in index 0
+        sender_text = senders[0].text
+    if(nsenders>2):
+        sys.stderr.write('{}: ERROR: Too many senders: {}, replaced with {}\n'.format(xmlfile, senders, default_sender))
+        if(error_is_fatal):
+            sys.exit(-1)
+        sender_text = default_sender
+
+    if(sender_text in sender_map):
+        sender_text_short = sender_map[sender_text]
+    else:
+        sys.stderr.write('{}: ERROR: Unmapped sender: {}, replaced with {}\n'.format(xmlfile, sender_text, default_sender))
+        if(error_is_fatal):
+            sys.exit(-1)
+        sender_text_short = default_sender
 
     categories = root.findall('./rdf:RDF/rdf:Description/mediapro:CatalogSets/rdf:Bag/rdf:li',ns)
     #print(categories)
@@ -60,34 +93,40 @@ def get_props(sender_map, xmlfile):
     for category in categories:
         categories_text.append(category.text)
 
-    return (headline, datecreated, senders_text, senders_text_short, categories_text)
+    return (headline, datecreated, sender_text, sender_text_short, categories_text)
 
-def get_new_filename(basedir, file, headline, datecreated, senders_text_short, categories_text):
+def get_new_filename(basedir, file, headline, datecreated, sender_text_short, categories_text):
     categories = ""
     for cat in categories_text:
         categories += "({})".format(cat.lower())
-    
+
     date_formatted = datecreated.replace('-','')[2:]
 
-    sender = senders_text_short[0]
-    p = Path('{}/{}'.format(basedir, sender))
+    p = Path('{}/{}'.format(basedir, sender_text_short))
     p.mkdir(exist_ok=True)
-    newfilename = "{}/{}/{}_{}_{} {}{}".format(basedir, sender, sender, date_formatted, headline, categories, file.suffix)
+    newfilename = "{}/{}/{}_{}_{} {}{}".format(basedir, sender_text_short, sender_text_short, date_formatted, headline, categories, file.suffix)
     return newfilename
 
 parser = argparse.ArgumentParser(description='Extract metadata from XMP files \n Extracts headline, datecreated, senders and categories. \n Errors (missing headline, missing datecreated, unmapped sender) re printed on sys.stderr')
 parser.add_argument("-r", "--rename-files", help="Rename files", action='store_const', const='rename_files')
+parser.add_argument("--display-metadata", help="Display metadata", action='store_const', const='display_metadata')
+parser.add_argument("--display-filename", help="Display new filename", action='store_const', const='display_metadata')
+parser.add_argument("--error-is-fatal", help="Exit with no action at error", action='store_const', const='error_is_fatal')
 parser.add_argument("sender_map", help="JSON mapping between sender and filname prefix")
 parser.add_argument("directory", help="Directory with XMP files")
 args = parser.parse_args()
+rename_files = args.rename_files
+display_metadata = args.display_metadata
+display_filename = args.display_filename
+error_is_fatal = args.error_is_fatal
 
 senders = json.load(open(args.sender_map, 'r'), encoding="utf-8")
 files = Path(args.directory).glob('*.XMP')
-rename_files = args.rename_files
 
 for file in files:
     props = get_props(senders, file)
-    print('metadata: {}:{}'.format(file, props))
+    if(display_metadata):
+        print('metadata: {}:{}'.format(file, props))
     (headline, datecreated, senders_text, senders_text_short, categories_text) = props
     file_base = PurePath(file).stem
     mediafile_all = Path(args.directory).glob('{}.*'.format(file_base))
@@ -95,7 +134,8 @@ for file in files:
     if len(mediafile_notxmp) > 0:
         mediafile = mediafile_notxmp[0]
         new_filename = get_new_filename(args.directory, mediafile, headline, datecreated, senders_text_short, categories_text)
-        print("-> rename {} -> {}".format(mediafile, new_filename))
+        if(display_filename):
+            print("-> rename {} -> {}".format(mediafile, new_filename))
         if(rename_files):
             mediafile.rename(new_filename)
 
